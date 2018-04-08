18b940d6b7188346d77a192c114e9e2a576ac4ae
KarlaLopez96}git config --global user.name KarlaLopez96 <00058615@uca.edu.sv>
2018-04-07 18:08:09 -0600

    Creando funciones para enviar texto plano.

--- app/src/main/java/com/karla00058615/acercade/MainActivity.java
+++ app/src/main/java/com/karla00058615/acercade/MainActivity.java
@@ -1,7 +1,11 @@
 package com.karla00058615.acercade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bt = (Button)findViewById(R.id.buttonShare);
+        bt.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plane");
+                String shareBody = "Karla Esperanza López \nIng.Informatica\nGitHubKarlaLopez96\nFacebook:Karla Lopez" +
+                        "\nCorreo:0005861@uca.edu.sv \n Cel:73459867";
+                String shareSub = "Your subject here";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT,shareSub);
+                myIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
+                startActivity(Intent.createChooser(myIntent,"Share using"));
+            }
+        });
     }
 }
